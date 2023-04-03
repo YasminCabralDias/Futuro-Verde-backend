b4080cadea3f0e73afdc3b29416979fae47f7397
YasminCabralDias <rm95800@fiap.com.br>
2023-04-03 18:54:59 -0300

    att

--- projeto-Digital-Business-Enablement-main/projeto-Digital-Business-Enablement-main/SystemModel/FuturoVerde-main/src/main/java/br/com/fiap/futuroverde/config/RestExceptionHandler.java
+++ projeto-Digital-Business-Enablement-main/projeto-Digital-Business-Enablement-main/SystemModel/FuturoVerde-main/src/main/java/br/com/fiap/futuroverde/config/RestExceptionHandler.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.server.ResponseStatusException;
 
 import br.com.fiap.futuroverde.models.RestError;
 import br.com.fiap.futuroverde.models.RestValidationError;
@@ -24,5 +25,11 @@ public class RestExceptionHandler {
         e.getFieldErrors().forEach(v -> erros.add(new RestValidationError(400, v.getField(), v.getDefaultMessage())));
         return ResponseEntity.badRequest().body(erros);
     }
+
+    @ExceptionHandler(ResponseStatusException.class)
+    public ResponseEntity<String> handleResponseStatusException(ResponseStatusException ex) {
+        return ResponseEntity.status(ex.getStatusCode()).body("Erro: " + ex.getReason());
+    }
+
 
 }

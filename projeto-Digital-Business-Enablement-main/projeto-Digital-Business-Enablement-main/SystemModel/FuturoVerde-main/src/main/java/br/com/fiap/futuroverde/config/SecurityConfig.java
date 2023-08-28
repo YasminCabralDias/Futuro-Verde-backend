1682e8714416e22c2f62b6bfd985ab96cb8d8d83
guilhermexit <rm93150@fiap.com.br>
2023-08-28 17:09:01 -0300

    Att

--- projeto-Digital-Business-Enablement-main/projeto-Digital-Business-Enablement-main/SystemModel/FuturoVerde-main/src/main/java/br/com/fiap/futuroverde/config/SecurityConfig.java
+++ projeto-Digital-Business-Enablement-main/projeto-Digital-Business-Enablement-main/SystemModel/FuturoVerde-main/src/main/java/br/com/fiap/futuroverde/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package br.com.fiap.futuroverde.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
@@ -12,21 +13,24 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-
 @Configuration
 public class SecurityConfig {
 
     @Autowired
     AuthorizationFilter authorizationFilter;
 
+    @Autowired
+    Environment env;
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
-        return http
+       
+        http
                 .authorizeHttpRequests()
                     .requestMatchers(HttpMethod.POST, "/api/registrar").permitAll()
                     .requestMatchers(HttpMethod.POST, "/api/login").permitAll()
                     .requestMatchers("/v3/api-docs/**", "/swagger-ui.html", "/swagger-ui/**" ).permitAll()
-                    .anyRequest().authenticated()
+                    
                 .and()
                 .csrf().disable()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
@@ -34,8 +38,15 @@ public class SecurityConfig {
                 .formLogin().disable()
                 .headers().frameOptions().sameOrigin()
                 .and()
-                .addFilterBefore(authorizationFilter, UsernamePasswordAuthenticationFilter.class)
-                .build();
+                .addFilterBefore(authorizationFilter, UsernamePasswordAuthenticationFilter.class);
+
+        if (env.getActiveProfiles().length > 0 && env.getActiveProfiles()[0].equals("open")){
+            http.authorizeHttpRequests().anyRequest().permitAll();
+        }else{
+            http.authorizeHttpRequests().anyRequest().authenticated();
+        }
+
+        return http.build();
     }
 
     @Bean
